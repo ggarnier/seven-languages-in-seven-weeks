7e419e6dadfa48242c79e8a9ac17ae8eb2ac6eb4
Guilherme Garnier <guilherme.garnier@gmail.com>
2015-09-28 23:33:07 -0500

    Add exercises from Prolog day 1

---
+++ prolog/day1/books.pl
@@ -0,0 +1,10 @@
+book(the_hitchhikers_guide_to_the_galaxy, douglas_adams).
+book(the_restaurant_at_the_end_of_the_universe, douglas_adams).
+book(life_the_universe_and_everything, douglas_adams).
+book(so_long_and_thanks_for_all_the_fish, douglas_adams).
+book(mostly_harmless, douglas_adams).
+book(the_lord_of_the_rings, tolkien).
+book(the_hobbit, tolkien).
+book(the_dilbert_principle, scott_adams).
+
+book(Title, douglas_adams).
---
+++ prolog/day1/music.pl
@@ -0,0 +1,8 @@
+plays(eric_clapton, guitar).
+plays(paul_mccartney, guitar).
+plays(brian_may, guitar).
+plays(ringo_starr, drums).
+plays(dave_grohl, guitar).
+plays(keith_moon, drums).
+
+plays(Musician, guitar).
